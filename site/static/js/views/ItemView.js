f02ac30fb05c574e1c46cd8d9827a8d6428ebee5
Barbara Chassoul <bachas90@gmail.com>
2015-03-01 13:03:34 -0600

    updating item-view

--- site/static/js/views/ItemView.js
+++ site/static/js/views/ItemView.js
@@ -2,26 +2,21 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'models/item',
-  'collections/item',
+  /*'models/item',
+  'collections/item',*/
   'text!html/fun-item.html'
-], function($, _, Backbone, ItemModel, ItemCollection, itemTemplate){
+], function($, _, Backbone, /*ItemModel, ItemCollection,*/ itemTemplate){
 
   var ItemView = Backbone.View.extend({
     el: $("#fun-item"),
-    
-    initialize: function(){
-      this.collection = new ItemCollection();
-      
-      var compiledTemplate = _.template( itemTemplate, { items: this.collection.models });
-      this.$el.html(compiledTemplate);
-    },
 
-  //  render: function(){
+    render: function(){
       
-  //    var compiledTemplate = _.template( itemTemplate );
-  //    $("#fun-item").append(compiledTemplate);
-  //  }
+      var compiledTemplate = _.template( itemTemplate );
+    
+      $("#fun-item").append(compiledTemplate);
+
+    }
 
   });
 
