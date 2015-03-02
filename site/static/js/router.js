6a037d01983cb00de6c60cf20c104512f4b958de
Barbara Chassoul <bachas90@gmail.com>
2015-03-01 18:22:41 -0600

    router

--- site/static/js/router.js
+++ site/static/js/router.js
@@ -11,7 +11,7 @@ define([
   var AppRouter = Backbone.Router.extend({
     routes: {
       // Default
-      '': 'home'
+      '*actions': 'defaultAction'
     }
   });
   
@@ -19,7 +19,7 @@ define([
 
     var app_router = new AppRouter;
     
-    app_router.on('home', function (actions) {
+    app_router.on('route:defaultAction', function (actions) {
      
        // We have no matching route, lets display the home page 
         var titleView = new TitleView();
