5b718cb1093c2f445ae935cdb89a2f34d0395f44
Barbara Chassoul <bachas90@gmail.com>
2015-04-07 09:42:36 -0600

    oh gosh

--- site/static/js/views/ListView.js
+++ site/static/js/views/ListView.js
@@ -10,50 +10,20 @@ define([
   var ListView = Backbone.View.extend({
     el: $("#fun-list"),
 
+    initialize: function() {
+      this.collection.on('sync', this.render, this);
+    },
+
     render: function(){
-      
       var compiledTemplate = _.template( listTemplate );
-    
       $("#fun-list").append(compiledTemplate);
       var itemView = new ItemView({collection: ItemsCollection});
-      itemView.render();
-      
+      this.$el.append(itemView.render().el);
     },
+
   });
+  
   return ListView;
   return ItemView;
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/****
-    render: function(){
-      
-      var compiledTemplate = _.template( listTemplate );
-    
-      $("#fun-list").append(compiledTemplate);
-      this.collection_.each(function(item){
-
-        var itemView = new ItemView({model: item});
-        element.append(itemView.render().el);
-
-      });
-      
-    },
-****/
